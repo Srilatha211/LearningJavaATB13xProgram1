5fecd5a239fd2469e3411cb1bb2a120df1f36d6b
srilatha <srilatha2930.k@gmail.com>
2025-06-05 20:50:52 +0530

    added stmt

--- src/ex_1_Java_Basic/Lab002_HW.java
+++ src/ex_1_Java_Basic/Lab002_HW.java
@@ -1,5 +1,7 @@
 package ex_1_Java_Basic;
 
 public class Lab002_HW {
-
+    public static void main(String[] args) {
+        System.out.println("Successfully pushed the code to github and able to view the code by solving the issue");
+    }
 }

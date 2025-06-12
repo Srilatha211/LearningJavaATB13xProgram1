a5fb735c94e2cfce80d2cc532c3b40ca5926fb3d
srilatha <srilatha2930.k@gmail.com>
2025-06-12 18:06:00 +0530

    12th June 2025

--- src/June_11th_Java_Basics/Lab0025_Bodmas.java
+++ src/June_11th_Java_Basics/Lab0025_Bodmas.java
@@ -3,7 +3,7 @@ package June_11th_Java_Basics;
 public class Lab0025_Bodmas {
 
     public static void main(String[] args) {
-        System.out.println((5*2/7+1)+6);
+        System.out.println((5*14/7+1)+6);
 
 
     }
---
+++ src/June_12_Java_Basics/Lab0039_Narrowing_Explicit.java
@@ -0,0 +1,17 @@
+package June_12_Java_Basics;
+
+public class Lab0039_Narrowing_Explicit {
+    public static void main(String[] args) {
+        double d= 400.5;
+        int i = (int)d;
+        System.out.println(i);
+
+        int a= 300;
+        byte b= (byte) a;
+        System.out.println(b);
+
+        //byte b=(byte)d;
+       //System.out.println(b);
+
+    }
+}

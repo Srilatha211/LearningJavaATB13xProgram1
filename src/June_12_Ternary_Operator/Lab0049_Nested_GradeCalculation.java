07ffe34e4baba30897ce4d695a7c52af1e419575
srilatha <srilatha2930.k@gmail.com>
2025-06-12 18:07:17 +0530

    12th June 2025 1

--- src/June_12_Ternary_Operator/Lab0049_Nested_GradeCalculation.java
+++ src/June_12_Ternary_Operator/Lab0049_Nested_GradeCalculation.java
@@ -8,7 +8,7 @@ public class Lab0049_Nested_GradeCalculation {
         //Description: Marks 90 = A+, 75 = A, 60 = B, 40 = C, below 40 Fail.
 
 //
-        int a= 100;
+        int a= 50;
         String result=(a>=90)?"A+":(a<90 && a>=75)?"A":(a<75 && a>=60)?"B":(a<60 && a>=40)?"C":"Fail";
         System.out.println(result);
 

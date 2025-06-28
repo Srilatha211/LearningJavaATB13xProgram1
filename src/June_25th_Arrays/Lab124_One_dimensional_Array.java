e28b5bc071313b010d53fc15cd39218ba016ff6e
srilatha <srilatha2930.k@gmail.com>
2025-06-28 15:59:22 +0530

    28th_June1

---
+++ src/June_25th_Arrays/Lab124_One_dimensional_Array.java
@@ -0,0 +1,10 @@
+package June_25th_Arrays;
+
+public class Lab124_One_dimensional_Array {
+    public static void main(String[] args) {
+        String[] names= {"sri","Latha","kore"};
+        System.out.println(names.length);
+        System.out.println(names[1]);
+    }
+
+}
---
+++ src/June_27th_2dimensional_Arrays/Lab135_Two_dimensional_Array_Creation.java
@@ -0,0 +1,37 @@
+package June_27th_2dimensional_Arrays;
+
+public class Lab135_Two_dimensional_Array_Creation {
+
+
+    public static void main(String[] args) {
+
+//Predefined array
+        int[][] array = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+
+//predefined array can be aslo in this form
+
+        int[][] array1;
+        array1= new int[][]{
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+//Fixed size array
+        int[] [] array2= new int[3][3];
+        array2 [0][0]= 1;
+        array2 [0][2]= 2;
+        array2 [0][3]= 3;
+        array2 [1][0]= 4;
+        array2 [1][2]= 5;
+        array2 [1][3]= 6;
+        array2 [2][0]= 7;
+        array2 [2][2]= 8;
+        array2 [2][3]= 9;
+
+
+    }
+}

6c23470714427507264fbdffee57ff7d55b340e7
srilatha <srilatha2930.k@gmail.com>
2025-06-30 18:55:57 +0530

    NewClass_java

--- src/June_25th_Arrays/NewClass.java
+++ src/June_25th_Arrays/NewClass.java
@@ -2,6 +2,6 @@ package June_25th_Arrays;
 
 public class NewClass {
     public static void main(String[] args) {
-        System.out.println("Helo");
+        System.out.println("Helo New");
     }
 }

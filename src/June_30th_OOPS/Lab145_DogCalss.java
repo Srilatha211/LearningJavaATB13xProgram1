3b105d671c29f1473f00343528237b2e6b5015e8
srilatha <srilatha2930.k@gmail.com>
2025-07-02 15:34:13 +0530

    Class

---
+++ src/June_30th_OOPS/Lab145_DogCalss.java
@@ -0,0 +1,16 @@
+package June_30th_OOPS;
+
+public class Lab145_DogCalss {
+    String name;
+    String breed;
+    String color_eyes;
+
+
+    void walk() {
+    }
+
+    void bark() {
+    }
+
+}
+
